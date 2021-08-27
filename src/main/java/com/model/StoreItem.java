d60742b4709a62ce5e62f5f99a967c3da2aab7ea
truonghq99 <truonghq.99@gmail.com>
2021-08-27 21:48:12 +0700

    r

--- src/main/java/com/model/StoreItem.java
+++ src/main/java/com/model/StoreItem.java
@@ -1,16 +1,12 @@
 package com.model;
 
-import java.io.Serializable;
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,16 +16,19 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class Store implements Serializable{
+public class StoreItem {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String address;
-    private String phoneNumber;
-    private Date openDate;
-    private String status;
 
-    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
-    private List<StoreItem> storeItem = new ArrayList<>();
-    
+    @ManyToOne(cascade=CascadeType.MERGE)
+    @JoinColumn(name="item_id",referencedColumnName = "id")
+    private Item item;
+    private int quantity;
+    private boolean active=true;
+
+    @ManyToOne(cascade=CascadeType.MERGE)
+    @JoinColumn(name="store_id",referencedColumnName = "id")
+    private Store store;
 }
